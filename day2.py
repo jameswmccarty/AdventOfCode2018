87cfbd736292c80a4dc1e9719d4ba233a8e6332f
jameswmccarty <jameswmccarty@gmail.com>
2018-12-02 18:42:39 -0500

    Update day2.py

--- day2.py
+++ day2.py
@@ -76,14 +76,10 @@ if __name__ == "__main__":
 			freq_gram = [0] * 26
 			for char in line.strip():
 				freq_gram[ord(char)-97] += 1
-			for count in freq_gram:
-				if count == 2:
-					twos += 1
-					break;
-			for count in freq_gram:
-				if count == 3:
-					threes += 1
-					break;
+			if 2 in freq_gram:
+				twos += 1
+			if 3 in freq_gram:
+				threes += 1
 	print "Part 1 solution: " + str(twos * threes)
 	
 	#Part 2 solution
@@ -96,11 +92,8 @@ if __name__ == "__main__":
 	while row_idx < len(all_lines) and not found:
 		for line in all_lines:
 			if editDist(all_lines[row_idx], line) == 1:
-				print line
-				print all_lines[row_idx]
+				#print line
+				#print all_lines[row_idx]
 				print min_str(line, all_lines[row_idx])
 				found = True
 		row_idx += 1
-		
-		
-
